e2dd426ab53ce1e1ded9aa124963a5c0f4b66be2
kyawswaraung <kyawswaaung87@gmail.com>
2020-09-20 11:59:24 +0630

    config server and client

---
+++ config-client/src/test/java/com/demo/config/client/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.config.client.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
